fe947b79640032e24550b829756962bfb9a0bfbe
d444n <53837142+d444n@users.noreply.github.com>
2019-08-17 20:12:30 +0200

    Add files via upload

---
+++ shop/index.php
@@ -0,0 +1,76 @@
+<?php
+include("src/header.php");
+
+
+?>
+  <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">autosell</h5>
+  
+  <a class="btn btn-outline-primary" href="#">Sign up</a>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#"></a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Log in</a>
+</div>
+
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <h1 class="display-4">autosell</h1>
+  <p class="lead">Quickly find a car or sell yours online. For private buyers and sellers.</p>
+</div>
+
+<div class="container">
+  <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">VW Golf</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$0</h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>attribut</li>
+          <li>attribut</li>
+          <li>attribut</li>
+          <li>attribut</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact the Seller</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Lamborgini Aventador</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$15</h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>attribut</li>
+          <li>attribut</li>
+          <li>attribut</li>
+          <li>attribut</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact the Seller</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Ford Kuga</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$29</h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>attribut</li>
+          <li>attribut</li>
+          <li>attribut</li>
+          <li>attribut</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact the Seller</button>
+      </div>
+    </div>
+  </div>
+<?php
+include("src/footer.php");
+?>
+ 
+</div>
+</body>
+</html>
